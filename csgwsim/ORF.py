f3009c7a9cfa8cd06717a14a679314d42c55555f
ekli-sysu <lienk@mail.sysu.edu.cn>
2023-09-15 18:08:19 +0800

    update orf

--- csgwsim/ORF.py
+++ csgwsim/ORF.py
@@ -9,4 +9,35 @@
 
 import numpy as np
 
+def get_uvk(lambd, beta):
+    snl = np.sin(lambd)
+    csl = np.cos(lambd)
+    snb = np.sin(beta)
+    csb = np.cos(beta)
+
+    u = np.array([snl, -csl, 0])
+    v = np.array([-snb * csl, - sinb*snl, csb])
+    k = np.array([-csb * csl, -snl*csb, -snb])
+    return (u,v,k)
+
+
+def polarization_tensor(u, v):
+    e_p = np.zeros((3,3))
+    e_c = np.zeros((3,3))
+
+    for i in range(3):
+        for j in range(3):
+            e_p[i,j] = u[i] * u[j] - v[i] * v[j]
+            e_c[i,j] = u[i] * v[j] + v[i] * u[j]
+
+    return (e_p, e_c)
+
+def transfer_single_arm(f, kn, kpr, kps, LT):
+    return (0.5 * np.sinc(np.pi * f *LT*(1-kn)) * 
+            np.exp(-1j * np.pi * f* (LT + kpr + kps)) )
+
+class OverlapReductionFunc(object):
+    '''
+    Calculate ORF
+    '''
 

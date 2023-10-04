894fcbc49b8b4d69f580923a0d9783e7cc7f93a8
Han Wang <wangh657@mail2.sysu.edu.cn>
2023-10-04 17:37:45 +0800

    convert gen signal to a class method; remove redundant attributes

--- gwspace/SGWB.py
+++ gwspace/SGWB.py
@@ -11,7 +11,7 @@ import healpy as hp
 from healpy import Alm
 from sympy.physics.quantum.cg import CG
 
-from gwspace import response
+from gwspace.response import trans_y_slr_fd, get_XYZ_fd
 from gwspace.Orbit import detectors
 from gwspace.wrap import frequency_noise_from_psd
 from gwspace.constants import H0_SI
@@ -20,84 +20,88 @@ from gwspace.constants import H0_SI
 class SGWB(object):
     """ Generate the anisotropy SGWB signal in frequency domain
 
-    :param fmax,fmin(Hz): the maximum and minimum frequency we generate
-    :param fn: the frequency segment number
-    :param tsegmid(s): the time segment length,for TQ we usually use 5000s or 3600s，The ORF error is less than 3%
-    :param Ttot(s): the total observe time, unit : second
+    :param nside: we usually use 8,12,16... The pixel number will be determined by (12 * nside**2)
     :param omega0: the intensity of the signal at reference frequency
     :param alpha: the power-law index
-    :param nside_in: determine the pixel number, we usually use 8,12,16...
     :param blm_vals: A series of spherical coefficients
     :param blmax: the order of blm_vals
-    :param fref(Hz): the reference frequency
     :param H0: Hubble constant
-    :param det: the detector type
     """
 
-    def __init__(self, fmax, fmin, fn, tsegmid, Ttot, omega0, alpha, nside_in, blm_vals, blmax,
-                 fref=0.01, H0=H0_SI, det="TQ"):
-        self.fmax = fmax
-        self.fmin = fmin
-        self.fn = fn
-        self.tsegmid = tsegmid
-        self.Ttot = Ttot
-        self.fref = fref
+    def __init__(self, nside, omega0, alpha, blm_vals, blmax, H0=H0_SI):
+        self.blm_size = Alm.getsize(blmax)
+        if len(blm_vals) != self.blm_size:
+            raise ValueError('The size of the input blm array does not match the size defined by lmax')
+
         self.omega0 = omega0
         self.alpha = alpha
-        self.nside_in = nside_in
-        self.blm_vals = blm_vals
+        self.blms = np.array(blm_vals, dtype=np.complex128)
         self.blmax = blmax
-        self.num_blms = Alm.getsize(self.blmax)
-        self.blms = np.zeros(self.num_blms, dtype='complex')
         self.H0 = H0
-        # self.detectors = detector
-        for ii in range(self.num_blms):
-            self.blms[ii] = complex(blm_vals[ii])
+
         self.almax = 2*self.blmax
         self.alm_size = (self.almax+1)**2
-        self.blm_size = Alm.getsize(self.blmax)
-        self.bl_idx = np.zeros(2*self.blm_size-self.blmax-1, dtype='int')
-        self.bm_idx = np.zeros(2*self.blm_size-self.blmax-1, dtype='int')
-        for ii in range(self.bl_idx.size):
-            # lval, mval = Alm.getlm(blmax, jj)
-            self.bl_idx[ii], self.bm_idx[ii] = self.idx_2_alm(blmax, ii)
-        alms_inj = self.blm_2_alm(self.blms)
+        self.bl_bm_idx = [self.idx_2_alm(self.blmax, ii) for ii in range(2*self.blm_size-self.blmax-1)]
+
+        beta_vals = self.calc_beta()
+        blm_full = self.calc_blm_full()
+        alms_inj = np.einsum('ijk,j,k', beta_vals, blm_full, blm_full)
         alms_inj2 = alms_inj/(alms_inj[0]*np.sqrt(4*np.pi))
         # extract only the non-negative components
         alms_non_neg = alms_inj2[0:hp.Alm.getsize(self.almax)]
 
-        self.skymap_inj = hp.alm2map(alms_non_neg, self.nside_in)
-        self.frange = np.linspace(self.fmin, self.fmax, self.fn)
-        tf = np.arange(0, Ttot, tsegmid)
-        Omegaf = self.omega0*(self.frange/self.fref)**self.alpha
-        Sgw = Omegaf*(3/(4*self.frange**3))*(self.H0/np.pi)**2
-        Sgw_Gu = frequency_noise_from_psd(Sgw, 1/Ttot, seed=123)
-        self.signal_in_Gu = np.einsum('i,j->ij', (2/Ttot)*Sgw_Gu*Sgw_Gu.conj(), self.skymap_inj)  # Gaussian signal
-        npix = hp.nside2npix(self.nside_in)
+        self.skymap_inj = hp.alm2map(alms_non_neg, nside)
+        self.npix = hp.nside2npix(nside)
         # Array of pixel indices
-        pix_idx = np.arange(npix)
+        pix_idx = np.arange(self.npix)
         # Angular coordinates of pixel indices
-        self.thetas, self.phis = hp.pix2ang(self.nside_in, pix_idx)
-        self.e_plus = np.einsum("ik,jk->ijk", self.vec_v, self.vec_v)-np.einsum("ik,jk->ijk", self.vec_u, self.vec_u)
-        self.e_cross = np.einsum("ik,jk->ijk", self.vec_u, self.vec_v)+np.einsum("ik,jk->ijk", self.vec_v, self.vec_u)
+        self.thetas, self.phis = hp.pix2ang(nside, pix_idx)
+    
+    def get_ori_signal(self, frange, Ttot, fref=0.01):
+        """
+
+        :param frange: the frequency range
+        :param Ttot: the total observation time (in seconds)
+        :param fref: (Hz) the reference frequency
+        :return:
+        """
+        Omegaf = self.omega0*(frange/fref)**self.alpha
+        Sgw = Omegaf*(3/(4*frange**3))*(self.H0/np.pi)**2
+        Sgw_Gu = frequency_noise_from_psd(Sgw, 1/Ttot, seed=123)
+        return np.einsum('i,j->ij', (2/Ttot)*Sgw_Gu*Sgw_Gu.conj(), self.skymap_inj)  # Gaussian signal
+
+    def get_response_signal(self, fmin, fmax, fn, tsegmid, Ttot, det='TQ'):
+        """
+
+        :param fmin: (Hz) the minimum frequency we generate
+        :param fmax: (Hz) the maximum frequency we generate
+        :param fn: the frequency segment number
+        :param tsegmid: the time segment length (in seconds), for TQ we usually use 5000s or 3600s,
+         The ORF error is less than 3%
+        :param Ttot: the total observation time (in seconds)
+        :param det: the detector type
+        :return:
+        """
+        frange = np.linspace(fmin, fmax, fn)
+        tf = np.arange(0, Ttot, tsegmid)
+        e_plus = np.einsum("ik,jk->ijk", self.vec_v, self.vec_v)-np.einsum("ik,jk->ijk", self.vec_u, self.vec_u)
+        e_cross = np.einsum("ik,jk->ijk", self.vec_u, self.vec_v)+np.einsum("ik,jk->ijk", self.vec_v, self.vec_u)
         det = detectors[det](tf)
-        # det = TianQinOrbit(tf)
-        self.TQresponse_plus = np.zeros((3, self.frange.size, tf.size, npix), dtype="complex")
-        self.TQresponse_cross = np.zeros((3, self.frange.size, tf.size, npix), dtype="complex")
-        for i in range(npix):
-            for j in range(self.frange.size):
-                TQreplus = response.trans_y_slr_fd(self.vec_k[:, i], self.e_plus[:, :, i], det, self.frange[j])[0]
-                TQxyz_plus = response.get_XYZ_fd(TQreplus, self.frange[j], det.L_T)
-                self.TQresponse_plus[:, j, :, i] = TQxyz_plus
-                TQrecross = response.trans_y_slr_fd(self.vec_k[:, i], self.e_cross[:, :, i], det, self.frange[j])[0]
-                TQxyz_cross = response.get_XYZ_fd(TQrecross, self.frange[j], det.L_T)
-                self.TQresponse_cross[:, j, :, i] = TQxyz_cross
-        # TQ_ORF is 3*3*frequency*t*pixel
-        self.TQ_ORF = (1/(8*np.pi))*(np.einsum("mjkl,njkl->mnjkl", np.conj(self.TQresponse_plus), self.TQresponse_plus)
-                                     + np.einsum("mjkl,njkl->mnjkl", np.conj(self.TQresponse_cross), self.TQresponse_cross))/(
-                                  2*np.pi*self.frange[None, None, :, None, None]*det.L_T)**2
-        response_signal = self.signal_in_Gu[None, None, :, None, :]*self.TQ_ORF
-        self.signal_SGWB = (4*np.pi)*np.sum(response_signal, axis=4)/npix
+        det_res_plus = np.zeros((3, fn, tf.size, self.npix), dtype="complex")
+        det_res_cross = np.zeros((3, fn, tf.size, self.npix), dtype="complex")
+        for i in range(self.npix):
+            for j in range(fn):  # TODO: polish this
+                y_plus, y_cross = trans_y_slr_fd(self.vec_k[:, i], (e_plus[:, :, i], e_cross[:, :, i]), det, frange[j])
+                det_res_plus[:, j, :, i] = get_XYZ_fd(y_plus, frange[j], det.L_T)
+                det_res_cross[:, j, :, i] = get_XYZ_fd(y_cross, frange[j], det.L_T)
+        # det_ORF is 3*3*frequency*t*pixel
+        det_ORF = (1/(8*np.pi))*(np.einsum("mjkl,njkl->mnjkl", np.conj(det_res_plus), det_res_plus)
+                                 + np.einsum("mjkl,njkl->mnjkl", np.conj(det_res_cross), det_res_cross))/(
+                                  2*np.pi*frange[None, None, :, None, None]*det.L_T)**2
+        signal_in_gu = self.get_ori_signal(frange, Ttot)
+        res_signal = signal_in_gu[None, None, :, None, :]*det_ORF
+        res_signal = (4*np.pi)*np.sum(res_signal, axis=4)/self.npix
+        return res_signal, frange
 
     def calc_beta(self):
         beta_vals = np.zeros((self.alm_size, 2*self.blm_size-self.blmax-1, 2*self.blm_size-self.blmax-1))
@@ -117,35 +121,22 @@ class SGWB(object):
 
         return beta_vals
 
-    def calc_blm_full(self, blms_in):
+    def calc_blm_full(self):
+        """convert blm array into a full blm array with -m values too"""
         # Array of blm values for both +ve and -ve indices
         blms_full = np.zeros(2*self.blm_size-self.blmax-1, dtype='complex')
 
         for jj in range(blms_full.size):
-
-            lval, mval = self.bl_idx[jj], self.bm_idx[jj]
+            lval, mval = self.bl_bm_idx[jj]
 
             if mval >= 0:
-                blms_full[jj] = blms_in[Alm.getidx(self.blmax, lval, mval)]
+                blms_full[jj] = self.blms[Alm.getidx(self.blmax, lval, mval)]
 
             elif mval < 0:
                 mval = -mval
-                blms_full[jj] = (-1)**mval * np.conj(blms_in[Alm.getidx(self.blmax, lval, mval)])
+                blms_full[jj] = (-1)**mval * np.conj(self.blms[Alm.getidx(self.blmax, lval, mval)])
 
         return blms_full
-
-    def blm_2_alm(self, blms_in):
-
-        beta_vals = self.calc_beta()
-        if blms_in.size != self.blm_size:
-            raise ValueError('The size of the input blm array does not match the size defined by lmax ')
-
-        # convert blm array into a full blm array with -m values too
-        blm_full = self.calc_blm_full(blms_in)
-
-        alm_vals = np.einsum('ijk,j,k', beta_vals, blm_full, blm_full)
-
-        return alm_vals
 
     @staticmethod
     def idx_2_alm(lmax, ii):
@@ -170,7 +161,7 @@ class SGWB(object):
 
     @property
     def vec_v(self):
-        return np.array([np.cos(self.phis)*np.cos(self.thetas),
+        return np.array([np.cos(self.thetas)*np.cos(self.phis),
                          np.cos(self.thetas)*np.sin(self.phis),
                          -np.sin(self.thetas)])
 
--- tests/test_SGWB.py
+++ tests/test_SGWB.py
@@ -14,34 +14,38 @@ import time
 from gwspace.Noise import TianQinNoise
 from gwspace.SGWB import SGWB
 
-SGWBpars = {"fmax": 0.2,
-            "fmin": 0.001,
-            "fn": 200,
-            "tsegmid": 5000,
-            "Ttot": 63*5000,
+SGWBpars = {"nside": 8,
             "omega0": 5e-11,
             "alpha": 0.667,
-            "nside_in": 8,
             "blm_vals": (1.0, 0.75, 0.5, 0.7j, 0.7-0.3j, 1.1j),
             "blmax": 2
             }
+signal_pars = {"fmax": 0.2,
+               "fmin": 0.001,
+               "fn": 200,
+               "tsegmid": 5000,
+               "Ttot": 63*5000,
+               }
 st = time.time()
 SGWB_signal = SGWB(**SGWBpars)
+res_signal, frange = SGWB_signal.get_response_signal(**signal_pars)
 ed = time.time()
 print(f"Time cost: {ed-st} s")
 
-hp.mollview(SGWB_signal.signal_in_Gu[0, :], title="The response signal")
+signal_in_gu = SGWB_signal.get_ori_signal(frange, signal_pars["Ttot"])
+hp.mollview(signal_in_gu[0, :], title="The response signal")
 
 tq = TianQinNoise()
-TX, TXY = tq.noise_XYZ(SGWB_signal.frange, unit="displacement")/(2e8*np.sqrt(3))**2
+TX, TXY = tq.noise_XYZ(frange, unit="displacement")/(2e8*np.sqrt(3))**2
 
 plt.figure()
-plt.loglog(SGWB_signal.frange, TX, label="XYZ channels noise PSD")
-plt.loglog(SGWB_signal.frange, SGWB_signal.signal_SGWB[0, 0, :, 0], label="inject signal sample")
+plt.loglog(frange, TX, label="XYZ channels noise PSD")
+plt.loglog(frange, res_signal[0, 0, :, 0], label="inject signal sample")
 plt.xlabel('frequency[Hz]')
 plt.ylabel('PSD [Hz]')
 plt.legend(loc='best')
 plt.show()
+
 # one year SNR for SGWB
-SNR = np.sqrt(24*3600*3.64*np.sum(SGWB_signal.signal_SGWB[0, 0, :, 0]**2/TX**2)/SGWB_signal.frange.size)
+SNR = np.sqrt(24*3600*3.64*np.sum(res_signal[0, 0, :, 0]**2/TX**2)/frange.size)
 print(SNR)
